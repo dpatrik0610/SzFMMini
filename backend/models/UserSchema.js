c41f6561b3b2306244d02d2de73547dbacfe2c1c
dpatrik0610 <d.patrik0610@gmail.com>
2023-09-24 17:05:29 +0200

    Created a basic User Schema and a User Repository Model for handling Database interactions

---
+++ backend/models/UserSchema.js
@@ -0,0 +1,17 @@
+const userSchema = {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    registration_date: { type: Date, default: new Date() },
+    last_login_date: { type: Date, default: new Date() },
+    planted_trees: [
+      {
+        tree_id: { type: ObjectID, ref: 'Tree' },
+        plant_date: { type: Date, required: true },
+        tree_type: { type: String, required: true },
+        tree_state: { type: String, default: 'seedling' },
+        last_sprinkled: {type: Date, required: true}
+      },
+    ],
+};
+
+module.exports = { userSchema };
---
+++ backend/models/db.js
@@ -0,0 +1,34 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
+
+class Database {
+  constructor() {
+    this.client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      await this.client.db("TreeSimulator").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      throw error;
+    }
+  }
+
+  getDb() {
+    return this.client.db();
+  }
+}
+
+const database = new Database();
+
+module.exports = database;
